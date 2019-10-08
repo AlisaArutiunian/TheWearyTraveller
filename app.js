7811ad15402d23e837d8229d2d68b3880a593880
Alisa <alicearutyunyan@yahoo.com>
2019-10-07 23:15:31 -0400

    Working views (handlebars), all info is static

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const exphbs = require('express-handlebars')
+const app = express();
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+app.get("/",(req,res)=>
+{
+
+  res.render("home")
+
+});
+
+app.get("/registration",(req,res)=>
+{
+
+    res.render("registration")
+
+});
+
+app.get("/roomListing",(req,res)=>
+{
+    res.render("roomListing")
+
+});
+
+
+const PORT = 3000;
+
+app.listen(3000, ()=>{
+    console.log(`Web server is connected at PORT${PORT}`)
+})
