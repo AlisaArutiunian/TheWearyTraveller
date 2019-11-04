d66545d9ec10db49069239cb9d38ea6745fb289c
Alisa <alicearutyunyan@yahoo.com>
2019-11-04 17:41:23 -0500

    Added Server Side validation and Database connectivity

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//routers
+
+const logRouter = require('./routes/login');
+const regRouter = require('./routes/registration');
+app.use('/login', logRouter);
+app.use('/registration', regRouter);
+
+
+
+
+
+const DBURL= "mongodb://heroku_2tzfl4n9:ck2uqofbtmqcldrhnnju92l7gh@ds241278.mlab.com:41278/heroku_2tzfl4n9";
+mongoose.connect(DBURL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log(`Database is connected`);
+  }).catch((err) => {
+    console.log(`Something went wrong : ${err}`);
+  });
 
 
 app.get("/",(req,res)=>
@@ -14,18 +37,6 @@ app.get("/",(req,res)=>
 
 });
 
-app.get("/registration",(req,res)=>
-{
-
-    res.render("registration")
-
-});
-
-app.get("/login",(req,res)=>
-{
-    res.render("login")
-
-});
 
 
 app.get("/roomListing",(req,res)=>
