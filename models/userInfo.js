6e6a487473f6372740472b78d05490b1ca1ef56b
Alisa IT <alicearutyunyan@yahoo.com>
2019-12-06 16:25:56 -0500

    added user registatration functionality

--- models/userInfo.js
+++ models/userInfo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+
 
 const userSchema = new Schema({
 
@@ -8,9 +10,24 @@ const userSchema = new Schema({
     firstname: {type:String, required: true},
     lastname: {type:String, required: true},
     password: {type:String, required: true},
+    //password2: {type:String, required: true},
     birthday: {type:Date, required: true},
     
 })
+
+// hashing/salting before saving the new object to DB
+
+userSchema.pre("save", function(next){
+    bcrypt.genSalt(10)
+    .then(salt=>{
+        bcrypt.hash((this.password), salt)
+        .then(hash=>{
+            this.password=hash;
+            next();
+        })
+    })
+})
+
 
 const UserInfo = mongoose.model('UserInfo',userSchema );
 
--- routes/registration.js
+++ routes/registration.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const UserInfo = require('../models/userInfo');
 const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
@@ -22,16 +23,19 @@ router.post('/', function(req, res) {
       firstname: [],
       lastname: [],
       password: [],
+      password2: [],
       birthday: []
     };
   
     if (req.body.inEmail === '') {
       error.email.push('Please enter your email address');
-    }
+    } 
+  
 
     if (req.body.inFirstName === '') {
       error.firstname.push('Please enter your first name');
-    }
+    } 
+  
 
     if (req.body.inLastName === '') {
       error.lastname.push('Please enter your last name');
@@ -40,8 +44,6 @@ router.post('/', function(req, res) {
     if (req.body.inBirthday === '') {
       error.birthday.push('Please enter your birthday');
     }
-
-    
   
     if (req.body.inPassword === '') {
       error.password.push('Please enter your password');
@@ -53,14 +55,29 @@ router.post('/', function(req, res) {
       if(!checkCaps.test(req.body.inPassword))
       {
         error.password.push('Password should contain at least one upper case letter');
+      } 
+      if(req.body.inPassword.length < 8)
+      {
+        error.password.push('Password should be at least 8 characters');
       }
     }
-  
+
+    if (req.body.inputConfirmPassword === '')
+    {
+      error.password2.push('Please confirm your password');
+    } else {
+          if (req.body.inputConfirmPassword !== req.body.inPassword)
+          {
+            error.password2.push(`Passwords don't match!`);
+          }
+    }
+
     if (error.email.length > 0 ||
       error.firstname.length > 0 ||
       error.lastname.length > 0 ||
       error.password.length > 0 ||
-      error.birthday.length) {
+      error.birthday.length > 0 ||
+      error.password2.length > 0) {
   
       res.render('home', {
      
@@ -73,12 +90,28 @@ router.post('/', function(req, res) {
       });
   
     } else {
+      UserInfo.findOne({email:req.body.inEmail})
+      .then(user =>{
+        if(user) {
+          //user exists
+          error.email.push('This email is already registered');
+          res.render('home', {
+            regDisplay: true,
+            outEmail: req.body.inEmail,
+            outFirstName: req.body.inFirstName,
+            outLastName: req.body.inLastName,
+            outBirthday: req.body.inBirthday,
+            error: error,
+          });
+      }  else {
 
       const User = new UserInfo({
-        email: req.body.inEmail,
+        
+      email: req.body.inEmail,
       firstname: req.body.inFirstName,
       lastname: req.body.inLastName,
       password: req.body.inPassword,
+      password2: req.body.inputConfirmPassword,
       birthday: req.body.inBirthday
  
       });
@@ -114,12 +147,10 @@ router.post('/', function(req, res) {
       }).catch((err) => {
         console.log(`Houston, we have a problem: ${err}`);
       });
-
-      res.redirect('/');
-    }
-  
-  });
-
-
+          res.redirect('/');
+       }
+      });
+     }
+    });
 
   module.exports = router;
